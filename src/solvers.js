e25d7eb62438df378c9e0e4c7b3eeb6bf1b94055
Hack Reactor Student <student@hackreactor.com>
2017-04-06 15:36:37 -0700

    solve nRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,49 +16,68 @@
 
 
 window.findNRooksSolution = function(n) {
-  //var solution = undefined; //fixme
-
-  var board = new Board({n: n});
-
-  var recurse = function(board, nR, rI, cI) {
-
-    var nextSquare = function() {
-      if (cI >= n-1) {
-        rI+1;
-        cI = 0;
-      } else {
-        cI+1;
-      }
-    };
-
-    if (nR === n) {
-      if (!board.hasAnyRooksConflicts) {
-        return board;
-      }
-
-    
-
-
+  var newBoard = new Board({n: n});
+  var nextSpace = function(rI, cI, n) {
+    if (cI === n - 1) {
+      rI += 1;
+      cI = 0;
     } else {
-    togglePiece(rI, cI);
-    nextSquare();
-    recurse(board, nR, rI, cI);
-
+      cI += 1;
     }
-
-
+    return [rI, cI];
   }
 
-  recurse(board, 0, rI, cI);
-  console.log('solutions for NRooks: ', count);
+  var recurse = function(board, rN, rI, cI) {
+    if (rN !== n) {
+      board.togglePiece(rI, cI);
+      if (!board.hasAnyRooksConflicts()) {
+        rN++
+      } else {
+        board.togglePiece(rI, cI);
+      }
+      var newIndices = nextSpace(rI, cI, n);
+    } else {
+      return board.rows();
+    }
+    return recurse(board, rN, newIndices[0], newIndices[1])
+  }
+  var solution = recurse(newBoard, 0, 0, 0);
+  return solution;
 };
+
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var counter = 0;
+  var newBoard = new Board({n: n});
+  var nextSpace = function(rI, cI, n) {
+    if (cI === n - 1) {
+      rI += 1;
+      cI = 0;
+    } else {
+      cI += 1;
+    }
+    return [rI, cI];
+  };
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  var recurse = function(board, rN, rI, cI) {
+    if (rN !== n) {
+      board.togglePiece(rI, cI);
+      if (!board.hasAnyRooksConflicts()) {
+        rN++
+      } else {
+        board.togglePiece(rI, cI);
+      }
+      var newIndices = nextSpace(rI, cI, n);
+    } else {
+      counter++;
+      // return board.rows();
+    }
+    return recurse(board, rN, newIndices[0], newIndices[1])
+  };
+
+  recurse(newBoard, 0, 0, 0);
+  return counter;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
