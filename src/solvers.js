dddb2eea1f06dc8f83e3ec68b236b497ffe00aa1
Hack Reactor Student <student@hackreactor.com>
2017-04-06 19:59:48 -0700

    complete countNQueensSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -111,26 +111,103 @@ window.countNRooksSolutions = function(n) {
   return counter;
 };
 
+window.nextSpace = function(rI, cI, n) {
+  if (cI === n - 1) {
+    //rI +=1
+    rI += 1;
+    //cI = 0
+    cI = 0;
+  //else cI += 1; 
+  } else {
+    cI += 1;
+  }
+  return [rI, cI];
+};
+
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  console.log('Size: ' + n);
   var newBoard = new Board({n: n});
-  var nextSpace = function(rI, cI, n) {
-    if (cI === n - 1) {
-      if (rI === n - 1) {
-        return [rI, cI];
-      } else {
-        rI += 1;
-        cI = 0;
-      }
-    } else {
-      cI += 1;
-    }
-    return [rI, cI];
-  }
 
-  var recurse = function(board, rN, rI, cI) {
-    console.log('Rook Num:' + rN);
+  var recurse = function(board, qN, rI, cI) {
+    console.log('OUT' + rI);
+    if (qN === n) {
+      return board.rows(); 
+    }
+
+    if (rI > n - 1) {
+      console.log('IF' + rI);
+      return;
+    } else {
+      board.togglePiece(rI, cI);
+      if (!board.hasAnyQueensConflicts()) {
+        qN += 1; 
+        var newIndices = this.nextSpace(rI, cI, n);      //
+        var firstIt = recurse(board, qN, newIndices[0], newIndices[1]);
+        if (firstIt) {
+          return firstIt;
+        }
+        board.togglePiece(rI, cI);
+        qN--;
+        return recurse(board, qN, newIndices[0], newIndices[1]);    
+      } else {
+        board.togglePiece(rI, cI);
+        var newIndices = this.nextSpace(rI, cI, n);
+        return recurse(board, qN, newIndices[0], newIndices[1]);
+      }
+    }  
+    return recurse(board, qN, newIndices[0], newIndices[1]);
+  };
+  var solution = recurse(newBoard, 0, 0, 0);
+
+  return solution;
+};
+
+// CONFLICTS 1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,1
+
+// return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
+window.countNQueensSolutions = function(n) {
+  var solutionCount = 0; 
+
+  var newBoard = new Board({n: n});
+  // create a function called addRook(board, rookNum, rI, cI)
+  var recurse = function (board, qN, rI, cI) {
+    if (qN === n) {
+      solutionCount++;
+      return;
+    }
+    if (rI > n - 1) {
+      return;
+    //base case: rookNum = n
+    // continue if: rookNum < n
+    } else {
+      //toggle rook at location(rI, cI)
+      board.togglePiece(rI, cI);
+      // if valid rook: !hasAnyRowConflicts && !hasAnyColConflicts
+      if (!board.hasAnyQueensConflicts()) {
+        // increment rookNum
+        qN += 1; 
+        var newIndices = this.nextSpace(rI, cI, n);      //
+        recurse(board, qN, newIndices[0], newIndices[1]);
+        //delete existing rook & start a new
+        board.togglePiece(rI, cI);
+        qN--;
+        recurse(board, qN, newIndices[0], newIndices[1]);    
+      } else {
+        //toggle rook
+        board.togglePiece(rI, cI);
+        var newIndices = this.nextSpace(rI, cI, n);
+        recurse(board, qN, newIndices[0], newIndices[1]);
+      } 
+    }  
+    // recusive call (board, rookNum, rI, cI)
+  };
+  recurse(newBoard, 0, 0, 0);
+
+  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  return solutionCount;
+};
+
+
     // console.log('tuple:', rI, cI);
   //   if (rN !== n) {
   //     console.log('IF LOOP');
@@ -164,17 +241,3 @@ window.findNQueensSolution = function(n) {
   //   //   return;
   //   // }
   // //   }
-  }
-  console.log(solution);
-  return solution;
-};
-
-// CONFLICTS 1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,1
-
-// return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
-};
